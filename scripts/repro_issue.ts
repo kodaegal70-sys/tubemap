eebf48511443a7caa956fcf27bb4b2e5c303d4d4
Tube Map Dev <kodaegal70@gmail.com>
2026-01-31 15:00:48 +0900

    빌드 오류 수정: scripts 제외 및 노후 파일 추가 정리

--- scripts/repro_issue.ts
+++
@@ -1,9 +0,0 @@
-
-import { exec } from 'child_process';
-console.log("Running test_curl.ps1 via Node exec...");
-exec('powershell -ExecutionPolicy Bypass -File scripts/test_curl.ps1', (err, stdout, stderr) => {
-    if (err) console.error("EXEC ERROR:", err);
-    console.log('--- STDERR ---');
-    console.log(stderr);
-    console.log('--- END STDERR ---');
-});
